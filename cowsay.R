e8398569c8deeb9cfcc2570d5019113bf23d125e
bu11ymaguire <134497208+bu11ymaguire@users.noreply.github.com>
2025-07-20 21:32:59 +0900

    Create cowsay.R

---
+++ cowsay.R
@@ -0,0 +1,20 @@
+install.packages('cowsay')
+
+library(cowsay)
+say('Hello World!',by='cat')
+
+# < Hello World! >
+# -------------- 
+#         \
+#          \
+#
+#            |\___/|
+#          ==) ^Y^ (==
+#            \  ^  /
+#             )=*=(
+#            /     \
+#            |     |
+#           /| | | |\
+#           \| | |_|/\
+#      jgs  //_// ___/
+#               \_)
